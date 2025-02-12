43d849f710ac2d3e64f055faac8b6d971a33223e
Novgeny Ramadhalero Ermiawan <dalero33ermiawan@gmail.com>
2025-02-12 23:19:46 +0900

    fix(database): perbaiki no default value tabel user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,9 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('no_hp');
-            $table->string('tempat_tgl_lahir');
-            $table->string('pekerjaan');
+            $table->string('no_hp')->nullable();
+            $table->string('tempat_tgl_lahir')->nullable();
+            $table->string('pekerjaan')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
