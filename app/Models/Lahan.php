f7848f0e36b4f51e7b5d262e13916f18126e6027
Novgeny Ramadhalero Ermiawan <dalero33ermiawan@gmail.com>
2025-02-13 22:15:58 +0900

    fix(model): menambahkan softdeletes

--- app/Models/Lahan.php
+++ app/Models/Lahan.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Lahan extends Model
 {
     //
+    use SoftDeletes;
     protected $fillable = [
         'name',
         'slug',
