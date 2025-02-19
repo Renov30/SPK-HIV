dcc7ef62dfca3147d11df87f653d3ec060f97e43
Novgeny Ramadhalero Ermiawan <dalero33ermiawan@gmail.com>
2025-02-20 01:28:42 +0900

    feat(admin): membuat agar tombol masuk membuka tab baru di browser

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,5 @@
     <!-- navbar start -->
-    <nav class="navbar"> 
+    <nav class="navbar" id="nav"> 
         <a href="#" class="navbar-logo">Peta<span>Jagung.</span></a>
         <div class="navbar-nav">
           <a href="{{route('front.index')}}">Home</a>
@@ -8,7 +8,7 @@
           <a href="{{route('front.peta')}}">Peta</a>
         </div>
         <div class="navbar-extra">
-          <a href="{{route('filament.admin.auth.login')}}" class="login">Masuk</a>
+          <a href="{{route('filament.admin.auth.login')}}" class="login" target="blank">Masuk</a>
           <a href="{{route('front.data')}}" id="hamburger-menu"><i data-feather="menu"></i></a>
         </div>
       </nav>
