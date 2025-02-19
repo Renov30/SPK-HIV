b4fc298d61a09aab95737de084baee8bf59fb1fd
Novgeny Ramadhalero Ermiawan <dalero33ermiawan@gmail.com>
2025-02-20 01:27:31 +0900

    refactor(js): merapihkan sedikit kode js

--- public/js/filament/script.js
+++ public/js/filament/script.js
@@ -21,15 +21,15 @@ const navbarNav = document.querySelector(".navbar-nav");
 const hamburger = document.querySelector("#hamburger-menu");
 
 hamburger.addEventListener("click", function (e) {
-  e.preventDefault(); // Mencegah kembali ke atas
-  navbarNav.classList.toggle("active");
+    e.preventDefault(); // Mencegah kembali ke atas
+    navbarNav.classList.toggle("active");
 });
 
 // Klik di luar navbar untuk menutupnya
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
-  }
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove("active");
+    }
 });
 
 const toggleViewBtn = document.getElementById("toggleViewBtn");
@@ -37,36 +37,37 @@ const cardView = document.getElementById("cardView");
 const tableView = document.getElementById("tableView");
 
 toggleViewBtn.addEventListener("click", () => {
-  cardView.classList.toggle("hidden");
-  tableView.classList.toggle("hidden");
-  if (cardView.classList.contains("hidden")) {
-    toggleViewBtn.id = "toggleViewBtnTable";
-    toggleViewBtn.innerHTML = '<i data-feather="image"></i>&nbsp;Table View';
-  } else {
-    toggleViewBtn.id = "toggleViewBtn";
-    toggleViewBtn.innerHTML = '<i data-feather="table"></i>&nbsp;Card View';
-  }
-  feather.replace();
+    cardView.classList.toggle("hidden");
+    tableView.classList.toggle("hidden");
+    if (cardView.classList.contains("hidden")) {
+        toggleViewBtn.id = "toggleViewBtnTable";
+        toggleViewBtn.innerHTML =
+            '<i data-feather="image"></i>&nbsp;Table View';
+    } else {
+        toggleViewBtn.id = "toggleViewBtn";
+        toggleViewBtn.innerHTML = '<i data-feather="table"></i>&nbsp;Card View';
+    }
+    feather.replace();
 });
 
 function openLightbox(src) {
-  document.getElementById("lightbox-img").src = src;
-  document.getElementById("lightbox").style.display = "flex";
-  document.getElementById("nav").style.zIndex = "0";
+    document.getElementById("lightbox-img").src = src;
+    document.getElementById("lightbox").style.display = "flex";
+    document.getElementById("nav").style.zIndex = "0";
 }
 
 function closeLightbox() {
-  document.getElementById("lightbox").style.display = "none";
-  document.getElementById("nav").style.zIndex = "9999";
+    document.getElementById("lightbox").style.display = "none";
+    document.getElementById("nav").style.zIndex = "9999";
 }
 
 function toggleGallery() {
-  const gallery = document.getElementById("gallery");
-  gallery.classList.toggle("rows");
-  const showMoreText = document.querySelector(".show-more");
-  if (gallery.classList.contains("rows")) {
-    showMoreText.textContent = "Show Less";
-  } else {
-    showMoreText.textContent = "Show More";
-  }
+    const gallery = document.getElementById("gallery");
+    gallery.classList.toggle("rows");
+    const showMoreText = document.querySelector(".show-more");
+    if (gallery.classList.contains("rows")) {
+        showMoreText.textContent = "Show Less";
+    } else {
+        showMoreText.textContent = "Show More";
+    }
 }
