a80f36f6701f7b368acd1f296928504816aac1d1
Novgeny Ramadhalero Ermiawan <dalero33ermiawan@gmail.com>
2025-02-21 05:41:17 +0900

    feat(peta): menambahkan fitur marker di google maps api

--- resources/views/front/peta.blade.php
+++ resources/views/front/peta.blade.php
@@ -13,7 +13,7 @@
                 <div id="map"></div>
                 <script>
                     function initMap() {
-                        var lokasi = { lat: -6.2, lng: 106.816666 }; // Contoh: Jakarta
+                        var lokasi = { lat: -8.4975434, lng: 140.3882068 }; // Contoh: Jakarta
                         var map = new google.maps.Map(
                             document.getElementById("map"),
                             {
@@ -34,6 +34,61 @@
         <x-footer/>
 @endsection
 @push('after-scripts')
+ <script>
+        function initMap() {
+            var mapOptions = {
+                zoom: 12,
+                center: { lat: -8.4975434, lng: 140.3882068  }, // Default: Jakarta
+            };
+
+            var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+            var activeInfoWindow = null; 
+
+            var locations = @json($lahans);
+
+            locations.forEach(function(lahan) {
+                var position = new google.maps.LatLng(lahan.latitude, lahan.longitude);
+
+                var contentString =
+                    '<div>' +
+                        '<h5>' + lahan.name + '</h5>' +
+                        '<p>' + lahan.alamat + '</p>' +
+                        '<a href="/data/detail-lahan/' + lahan.slug + '">Lihat Detail</a>' +
+                    '</div>';
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
+                var marker = new google.maps.Marker({
+                    position: position,
+                    map: map,
+                    title: lahan.name,
+                    icon: {
+                        url: "{{asset('img/corn-cob.png')}}", // Ganti dengan path gambar Anda
+                        scaledSize: new google.maps.Size(30, 30), // Ubah ukuran sesuai kebutuhan
+                    }
+                });
+
+                marker.addListener('click', function() {
+                    if (activeInfoWindow) {
+                        activeInfoWindow.close();
+                    }
+                    // map.setZoom(15);
+                    // map.setCenter(marker.getPosition());
+                    infowindow.open(map, marker);
+                    activeInfoWindow = infowindow;
+                });
+
+                google.maps.event.addListener(map, 'click', function() {
+                    if (activeInfoWindow) {
+                        activeInfoWindow.close();
+                        activeInfoWindow = null;
+                    }
+                });
+            });
+        }
+    </script>
     <script
         async
         defer
