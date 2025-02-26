3eb9a07ae98a60282b464fe0778d449d10c7ab7e
Novgeny Ramadhalero Ermiawan <dalero33ermiawan@gmail.com>
2025-02-27 00:28:19 +0900

    feat(produksi): membuat tampilan resource produksi

--- app/Filament/Resources/ProduksiResource.php
+++ app/Filament/Resources/ProduksiResource.php
@@ -6,9 +6,14 @@ use App\Filament\Resources\ProduksiResource\Pages;
 use App\Filament\Resources\ProduksiResource\RelationManagers;
 use App\Models\Produksi;
 use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -17,13 +22,39 @@ class ProduksiResource extends Resource
 {
     protected static ?string $model = Produksi::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $modelLabel = 'Produksi'; // Label untuk satu item
+    protected static ?string $pluralModelLabel = 'Daftar Hasil Produksi'; // Label untuk daftar item
+    protected static ?string $navigationLabel = 'Produksi'; // Label di sidebar
+
+    protected static ?string $navigationIcon = 'heroicon-o-archive-box';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 //
+                Repeater::make('')
+                    ->columnSpan('full')
+                    ->addActionLabel('Tambah Hasil Produksi')
+                    ->schema([
+                        Grid::make(2)
+                            ->schema([
+                                Select::make('lahan_id')
+                                    ->label('Lahan')
+                                    ->required()
+                                    ->relationship('lahan', 'name'),
+                                Select::make('tahun_produksi')
+                                    ->label('Tahun')
+                                    ->options(
+                                        array_combine(range(date('Y'), 2015), range(date('Y'), 2015))
+                                    )
+                                    ->required(),
+                                TextInput::make('hasil_produksi')
+                                    ->label('Jumlah Produksi')
+                                    ->suffix('ton')
+                                    ->required(),
+                            ])
+                    ]),
             ]);
     }
 
@@ -32,6 +63,18 @@ class ProduksiResource extends Resource
         return $table
             ->columns([
                 //
+                TextColumn::make('lahan.name')
+                    ->label('Nama Lahan')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('tahun_produksi')
+                    ->label('Tahun Produksi')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('hasil_produksi')
+                    ->label('Hasil Produksi')
+                    ->searchable()
+                    ->sortable(),
             ])
             ->filters([
                 Tables\Filters\TrashedFilter::make(),
