41557d65f5850920600ac55bcd4aef7fe92a3f55
Novgeny Ramadhalero Ermiawan <dalero33ermiawan@gmail.com>
2025-02-28 00:23:33 +0900

    feat(chart): mengubah penarikan data jumlah produksi per distrik

--- app/Filament/Widgets/JumlahProduksiChart.php
+++ app/Filament/Widgets/JumlahProduksiChart.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Lahan;
-use App\Models\Produksi;
 use Filament\Widgets\ChartWidget;
 
 class JumlahProduksiChart extends ChartWidget
@@ -19,17 +18,18 @@ class JumlahProduksiChart extends ChartWidget
 
     protected function getData(): array
     {
-        $data = Produksi::selectRaw('lahans.distrik_id, SUM(produksis.hasil_produksi) as total_produksi')
-            ->join('lahans', 'produksis.lahan_id', '=', 'lahans.id') // Join dengan tabel lahan
+        $data = Lahan::join('produksis', 'lahans.id', '=', 'produksis.lahan_id')
+            ->select('lahans.distrik_id')
+            ->selectRaw('SUM(produksis.hasil_produksi) as total_produksi')
             ->groupBy('lahans.distrik_id')
-            ->with('lahan.distrik') // Ambil data distrik
+            ->with('distrik')
             ->get();
 
         return [
-            'labels' => $data->map(fn($item) => $item->lahan->distrik->name)->toArray(), // Ambil nama distrik
+            'labels' => $data->pluck('distrik.name')->toArray(),
             'datasets' => [
                 [
-                    'data' => $data->pluck('total_produksi')->toArray(), // Ambil hasil produksi total
+                    'data' => $data->pluck('total_produksi')->toArray(),
                     'backgroundColor' => ['#ff6384', '#36a2eb', '#ffcd56'],
                 ],
             ],
