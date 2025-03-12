b07f2425b70b2e1ecdebc37dfcda85dd3bb6355b
Novgeny Ramadhalero Ermiawan <dalero33ermiawan@gmail.com>
2025-03-13 06:39:51 +0900

    feat(widget): add produksi kuartal widget on dashboard

--- app/Filament/Widgets/LatestLahan.php
+++ app/Filament/Widgets/LatestLahan.php
@@ -9,7 +9,7 @@ use Filament\Widgets\TableWidget as BaseWidget;
 
 class LatestLahan extends BaseWidget
 {
-    protected static ?int $sort = 3;
+    protected static ?int $sort = 4;
     protected int | string | array $columnSpan = 'full';
 
     protected static bool $isLazy = false;
